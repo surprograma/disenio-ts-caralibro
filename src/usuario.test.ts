e050803aa8d24b3d0072925ee5903c49be95721a
Federico Aloi <federico.aloi@gmail.com>
2021-08-23 20:23:28 -0300

    Movida inicialización a beforeEach

--- src/usuario.test.ts
+++ src/usuario.test.ts
@@ -1,11 +1,20 @@
-import { Foto, Texto } from "./publicacion";
+import { Foto, Publicacion, Texto } from "./publicacion";
 import { Usuario } from "./usuario";
 
 describe("Caralibro", () => {
-  const saludoCumpleanios = new Texto(
-    "Felicidades Pepito, que los cumplas muy feliz"
-  );
-  const fotoEnCuzco = new Foto(768, 1024);
+  let saludoCumpleanios: Publicacion, fotoEnCuzco: Publicacion;
+
+  /* 
+    Los objetos que se comparten entre tests los inicializamos dentro de un bloque `beforeEach`.
+    Si no lo hacemos así, los efectos de un test afectan a los demás y se vuelve cada vez más
+    complicado entender por qué está fallando.
+   */
+  beforeEach(() => {
+    saludoCumpleanios = new Texto(
+      "Felicidades Pepito, que los cumplas muy feliz"
+    );
+    fotoEnCuzco = new Foto(768, 1024);
+  });
 
   describe("Una publicación", () => {
     describe("de tipo foto", () => {
